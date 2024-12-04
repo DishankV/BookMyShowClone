e0cbac5154eceb510aea6628d65cea8003273636
DishankV <160301804+DishankV@users.noreply.github.com>
2024-12-04 12:19:10 +0000

    Updated basic routes

--- bms/src/App.js
+++ bms/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import {Routes,Route} from 'react-router-dom';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-blue-100">
-      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Homepage/>} />
+      <Route path="/movie/:id" element={<MoviePage/>} />
+      <Route path="/plays" element={<Playpage/>} />
+    </Routes>
   );
 }
 
