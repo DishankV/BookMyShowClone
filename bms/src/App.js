08460febe340feff188e0ad306282bef1ac50283
DishankV <160301804+DishankV@users.noreply.github.com>
2024-12-06 08:10:44 +0000

    Added 3 pages and updated code

--- bms/src/App.js
+++ bms/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import {Routes,Route} from 'react-router-dom';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import {Homepage,Moviepage,Playpage} from "../src/Pages"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage/>} />
-      <Route path="/movie/:id" element={<MoviePage/>} />
+      <Route path="/movie/:id" element={<Moviepage/>} />
       <Route path="/plays" element={<Playpage/>} />
     </Routes>
   );
